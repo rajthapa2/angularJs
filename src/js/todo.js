49c4a90656694f2b7a18d74f0f41004462e215d1
rajthapa2 <rajthapa@Rajs-MacBook-Pro.local>
2014-04-06 15:29:31 +0100

    added the add todo method

--- src/js/todo.js
+++ src/js/todo.js
@@ -1,7 +1,11 @@
-function TodoController($scope)
-{
-	$scope.totalToDos = 4;
-	$scope.todos = [{text: 'Learn AngularJs', done: false},
-					{text: 'Create an App', done: true}];
+	function TodoController($scope)
+	{
+		$scope.totalToDos = 4;
+		$scope.todos = [{text: 'Learn AngularJs', done: false},
+		{text: 'Create an App', done: true}];
 
-}
+		$scope.addTodo = function(){
+			$scope.todos.push({text:$scope.formTodoText, done: false});
+			$scope.formTodoText = '';
+		}
+	}
