18c7b791b507a5742f273d36aa9a9c0e1bd575d0
Raja Thapa <rthapa@comparethemarket.com>
2014-04-07 17:09:57 +0100

    <raja>added the remove function

--- src/js/todo.js
+++ src/js/todo.js
@@ -1,11 +1,19 @@
 	function TodoController($scope)
 	{
-		$scope.totalToDos = 4;
 		$scope.todos = [{text: 'Learn AngularJs', done: false},
 		{text: 'Create an App', done: true}];
+		
+		$scope.getTotalTodos = function(){
+			return $scope.todos.length;
+		};
+
 
 		$scope.addTodo = function(){
 			$scope.todos.push({text:$scope.formTodoText, done: false});
 			$scope.formTodoText = '';
+		}
+		$scope.removeTodo = function(todo){
+			var index = $scope.todos.indexOf(todo);
+			$scope.todos.splice(index, 1);
 		}
 	}
