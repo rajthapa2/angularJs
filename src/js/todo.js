42875a2da63e48c49a90d588f15acf1600e8e174
Raja Thapa <rthapa@comparethemarket.com>
2014-04-09 09:16:40 +0100

    added the remove of done tasks

--- src/js/todo.js
+++ src/js/todo.js
@@ -1,19 +1,29 @@
-	function TodoController($scope)
-	{
-		$scope.todos = [{text: 'Learn AngularJs', done: false},
-		{text: 'Create an App', done: true}];
-		
-		$scope.getTotalTodos = function(){
-			return $scope.todos.length;
-		};
+function TodoController($scope) {
+    $scope.todos = [{ text: 'Learn AngularJs', done: false },
+    { text: 'Create an App', done: true }];
+
+    $scope.getTotalTodos = function () {
+        return $scope.todos.length;
+    };
 
 
-		$scope.addTodo = function(){
-			$scope.todos.push({text:$scope.formTodoText, done: false});
-			$scope.formTodoText = '';
-		}
-		$scope.removeTodo = function(todo){
-			var index = $scope.todos.indexOf(todo);
-			$scope.todos.splice(index, 1);
-		}
-	}
+    $scope.addTodo = function () {
+        //add some tests around it
+        $scope.todos.push({ text: $scope.formTodoText, done: false });
+        $scope.formTodoText = '';
+    };
+
+    $scope.removeTodo = function (todo) {
+        var index = $scope.todos.indexOf(todo);
+        $scope.todos.splice(index, 1);
+    };
+
+    $scope.clearDoneTodos = function () {
+        for (var i = 0; i < $scope.todos.length; i++) {
+            if ($scope.todos[i].done === true) {
+                $scope.todos.splice(i, 1);
+                i--;
+            }
+        }
+    };
+}
